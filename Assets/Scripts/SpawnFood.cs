7a558026cdafc920c077a7e4bc021d9140f5919a
Demsy <demsyx707@gmail.com>
2015-11-26 19:56:09 +0700

    awal

---
+++ Assets/Scripts/SpawnFood.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnFood : MonoBehaviour
+{
+    // Food
+    public GameObject FoodPrefab;
+
+    // Borders
+    public Transform BorderTop;
+    public Transform BorderBottom;
+    public Transform BorderLeft;
+    public Transform BorderRight;
+    // Use this for initialization
+    void Start()
+    {
+        // Spawn food every 4 seconds, starting in 3
+        InvokeRepeating("Spawn", 3, 4);
+    }
+
+    // Spawn one piece of food
+    void Spawn()
+    {
+        // x position between left & right border
+        int x = (int)Random.Range(BorderLeft.position.x, BorderRight.position.x);
+
+        // y position between top & bottom border
+        int y = (int)Random.Range(BorderBottom.position.y, BorderTop.position.y);
+
+        // Instantiate the food at (x, y)
+        Instantiate(FoodPrefab, new Vector2(x, y), Quaternion.identity); // default rotation
+    }
+}
